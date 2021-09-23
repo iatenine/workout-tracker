5aec4084cfb88a32b085aa2a4d3f05bc7f41d7e0
Jack Linhart <iatenine@gmail.com>
2021-09-22 21:03:57 -0600

    Fixed duration field

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,20 +1,21 @@
 const router = require("express").Router();
-const path = require("path");
 const db = require("../models");
 
 // API Routes (prefixed with /api/)
 
 router.get("/workouts", async (req, res) => {
-  // Get all workouts in the database
-  console.log(mongoose.models.Workout);
   try {
-    const workouts = await mongoose.models.Workout.find({});
-    console.log(workouts);
-    // Send the workouts as JSON
+    // Get all workouts in database and return as JSON
+    const workouts = await db.Workouts.find({});
     res.json(workouts);
   } catch (err) {
     console.log(err);
   }
+});
+
+router.post("/workouts", async (req, res) => {
+  //What is this supposed to do?!?
+  console.log("workout POST ", req.body);
 });
 
 router.get("/workouts/range", async (req, res) => {
@@ -22,7 +23,7 @@ router.get("/workouts/range", async (req, res) => {
     const workoutAggregation = await db.Workout.aggregate([
       {
         $addFields: {
-          totalDuration: { $sum: "$workouts.duration" },
+          totalDuration: { $sum: "$exercises.duration" },
           totalWeight: { $sum: "$exercises.weight" },
           totalSets: { $sum: "$exercises.sets" },
           totalReps: { $sum: "$exercises.reps" },
